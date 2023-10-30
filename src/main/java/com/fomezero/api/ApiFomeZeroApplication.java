8ae808e24fc951247231605f75d3789c645b9817
Giovanna <giovannaevangelista78@gmail.com>
2023-10-30 11:16:07 -0300

    first commit

---
+++ src/main/java/com/fomezero/api/ApiFomeZeroApplication.java
@@ -0,0 +1,13 @@
+package com.fomezero.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFomeZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFomeZeroApplication.class, args);
+	}
+
+}
